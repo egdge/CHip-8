edf44b7c346028ed4d8e97e27af2dfab87429639
egdge <101757778+egdge@users.noreply.github.com>
2022-11-26 16:17:00 -0500

    Update SDL_Test.c

--- SDL_Test.c
+++ SDL_Test.c
@@ -1,6 +1,3 @@
-/*This source code copyrighted by Lazy Foo' Productions (2004-2022)
-and may not be redistributed without written permission.*/
-
 //Using SDL and standard IO
 #include <SDL2/SDL.h>
 #include <stdio.h>
